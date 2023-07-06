bd4275e9ff7136fb5643f9beb46d3ed96ed116ee
Lul4t3ch <lul4t3ch15@gmail.com>
2023-07-06 12:33:50 -0300

    validação do formulário com typescript

--- src/A0019-exercicio/form-control.ts
+++ src/A0019-exercicio/form-control.ts
@@ -1,4 +1,5 @@
-const SHOW_ERROR_MESSAGES = '.show-error-message';
+import isEmail from 'validator/lib/isEmail';
+const SHOW_ERROR_MESSAGES = 'show-error-message';
 
 const form = document.querySelector('.form') as HTMLFormElement;
 const username = document.querySelector('.username') as HTMLInputElement;
@@ -6,24 +7,59 @@ const email = document.querySelector('.email') as HTMLInputElement;
 const password = document.querySelector('.password') as HTMLInputElement;
 const password2 = document.querySelector('.password2') as HTMLInputElement;
 
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   hideErrorMessages(form);
-// });
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  hideErrorMessages(this);
+  checkForEmptyFields(username, email, password, password2);
+  checkEmail(email);
+  checkEqualPasswords(password, password2);
+  if (sendForm(this))
+    console.log({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+      password2: password2.value,
+    });
+});
 
-// function hideErrorMessages(form: HTMLFormElement): void {
-//   form
-//     .querySelectorAll('.' + SHOW_ERROR_MESSAGES)
-//     .forEach((item) => item.classList.remove(SHOW_ERROR_MESSAGES));
-// }
-// function showErrorMessage(input: HTMLInputElement, msg: string): void {
-//   const formFields = input.parentElement as HTMLDivElement;
-//   const errorMessage = formFields.querySelector(
-//     '.error-message',
-//   ) as HTMLSpanElement;
-//   errorMessage.innerText = msg;
-//   formFields.classList.add(SHOW_ERROR_MESSAGES);
-// }
+function checkForEmptyFields(...inputs: HTMLInputElement[]): void {
+  inputs.forEach((input) => {
+    if (!input.value) showErrorMessage(input, 'Field can not be empty.');
+  });
+}
 
-// showErrorMessage(username, 'MENSAGEM');
-// hideErrorMessages(form);
+function checkEmail(input: HTMLInputElement): void {
+  if (!isEmail(input.value)) showErrorMessage(input, 'Email is not valid!');
+}
+
+function checkEqualPasswords(
+  password: HTMLInputElement,
+  password2: HTMLInputElement,
+) {
+  if (password.value !== password2.value) {
+    showErrorMessage(password, 'Both passwords must be equal.');
+    showErrorMessage(password2, 'Both passwords must be equal.');
+  }
+}
+
+function hideErrorMessages(form: HTMLFormElement): void {
+  form
+    .querySelectorAll('.' + SHOW_ERROR_MESSAGES)
+    .forEach((item) => item.classList.remove(SHOW_ERROR_MESSAGES));
+}
+function showErrorMessage(input: HTMLInputElement, msg: string): void {
+  const formFields = input.parentElement as HTMLDivElement;
+  const errorMessage = formFields.querySelector(
+    '.error-message',
+  ) as HTMLSpanElement;
+  errorMessage.innerText = msg;
+  formFields.classList.add(SHOW_ERROR_MESSAGES);
+}
+
+function sendForm(form: HTMLFormElement): boolean {
+  let send = true;
+  form
+    .querySelectorAll('.' + SHOW_ERROR_MESSAGES)
+    .forEach(() => (send = false));
+  return send;
+}
